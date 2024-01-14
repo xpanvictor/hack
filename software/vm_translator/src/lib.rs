40738e3053f4fde698deac7b325a7238d48a27a3
xpanvictor <xpanvictor@gmail.com>
2024-01-14 18:25:35 +0100

    bug(parser): breaking down the source code into iter which is consumed line by line breaks

--- software/vm_translator/src/lib.rs
+++ software/vm_translator/src/lib.rs
@@ -19,17 +19,15 @@ pub fn vm_translator(mut args: impl Iterator<Item = String>) {
     let filepath = args.next().expect("Filepath is required!");
     let output_filepath = format!("{}.asm", filepath.rsplit_once(".").unwrap().0);
 
-    let mut code_writer = CodeWriter::new(&output_filepath);
-    let mut vm_parser_rc = Rc::new(RefCell::new(Parser::new(&filepath)));
-
-    // let vm_parser = Rc::clone(&vm_parser_rc);
-
-    while Rc::clone(&vm_parser_rc).borrow().has_more_lines() {
-        let cm = Rc::clone(&vm_parser_rc);
-        let mut cmb = cm.borrow_mut();
-        cmb.advance();
-        // let curr = cmb.current_command;
-        println!("{}", Rc::strong_count(&vm_parser_rc));
-    }
+    // let mut code_writer = CodeWriter::new(&output_filepath);
+    // let parser = Parser::new(&filepath);
+    // let mut vm_parser_rc = Rc::new(RefCell::new(&parser));
+    //
+    // while vm_parser_rc.borrow().has_more_lines() {
+    //     let vm_parser = Rc::clone(&vm_parser_rc);
+    //     let mut vm_parser = vm_parser.borrow_mut();
+    //     // vm_parser.advance();
+    //     Rc::clone(&vm_parser_rc);
+    // }
 
 }
--- software/vm_translator/src/parser.rs
+++ software/vm_translator/src/parser.rs
@@ -1,4 +1,5 @@
-use std::fs;
+use std::{fs, str::Lines};
+use std::cell::RefCell;
 
 pub struct ArgumentPair<'a> {
     first: &'a str,
@@ -20,26 +21,47 @@ pub enum CommandType<'a> {
 
 pub struct Parser<'a> {
     vm_source_code: String,
-    pub current_command: &'a str,
+    pub current_command: String,
     current_line_no: u32,
-    total_file_line_no: u32,
+    // total_file_line_no: u32,
 }
 
-impl<'a> Parser<'a> {
-    pub fn new(filepath: &str) -> Parser<'a> {
+/*
+impl<'a> IntoIterator for &'a Parser<'a> {
+type Item = &'a str;
+type IntoIter = ParserIterator<'a>;
+
+fn into_iter(self) -> Self::IntoIter {
+todo!()
+}
+}
+
+struct ParserIterator<'a> {
+commands_total: &'a Parser<'a>
+}
+
+impl<'a> Iterator for ParserIterator<'a> {
+type Item = &'a str;
+
+fn next(&mut self) -> Option<Self::Item> {
+return self.commands_total.vm_source_code.lines().
+}
+}
+*/
+
+impl<'a, 'b> Parser<'b> {
+    pub fn new(filepath: &str) -> Parser<'b> {
         println!("Reading vm file: {}", filepath);
-        let mut parser = Parser {
-            vm_source_code: fs::read_to_string(filepath).expect("Couldn't read from file!"),
-            current_command: "",
+        Parser {
+            vm_source_code: fs::read_to_string(filepath)
+                .expect("Couldn't read the source code"),
             current_line_no: 0,
-            total_file_line_no: 0,
-        };
-        parser.total_file_line_no = parser.vm_source_code.lines().count() as u32;
-        parser
+            current_command: "".to_owned()
+        }
     }
 
     /// Just strips out whitespace and comments
-    fn clean_line<'b>(line: &'b str) -> &'b str {
+    fn clean_line(line: &str) -> &str {
         let mut line = line.trim();
         if line.contains("//") {
             line = line.split_once("//").unwrap().0;
@@ -49,24 +71,25 @@ impl<'a> Parser<'a> {
     }
 
     pub fn has_more_lines(&'a self) -> bool {
-        self.current_line_no < self.total_file_line_no
+        self.vm_source_code.lines().peekable().peek().is_some()
     }
 
     /// # Panics
     /// Should be called only if has_more_lines() returns true
     pub fn advance(&'a mut self) {
         if !self.has_more_lines() {
-            panic!("Can't advance since no more vm code to interprete.")
+            panic!("Can't advance since no more vm code to interpret.")
         };
         let mut is_appropriate_line = false;
         while !is_appropriate_line {
-            self.current_command = self
-                .vm_source_code
-                .lines()
-                .nth(self.current_line_no as usize)
-                .expect("Error fetching next command");
-            self.current_line_no += 1;
-            self.current_command = Self::clean_line(&self.current_command);
+            // self.current_command = self
+            //     .vm_source_code
+            //     .borrow_mut()
+            //     .next()
+            //     .expect("")
+            //     .to_owned();
+            // self.current_line_no += 1;
+            // self.current_command = Self::clean_line(&self.current_command);
             if self.current_command.is_empty() {
                 is_appropriate_line = true;
             }
@@ -77,66 +100,66 @@ impl<'a> Parser<'a> {
         // }
     }
 
-    /// # Panics
-    /// if command type can't be deciphered
-    pub fn command_type(self) -> CommandType<'a> {
-        let mut command_list = self.current_command.split_ascii_whitespace();
-        let command = command_list
-            .next()
-            .expect("Command section of current instruction couldn't be extracted");
-
-        match command {
-            "and" | "or" | "sub" | "eq" | "neg" | "gt" | "lt" | "not" | "add" => {
-                CommandType::C_ARITHMETIC(command)
-            }
-            "push" => CommandType::C_PUSH(ArgumentPair {
-                first: command_list
-                    .next()
-                    .expect("First argument of push is expected!"),
-                second: command_list
-                    .next()
-                    .expect("Second argument of push is expected!")
-                    .parse()
-                    .unwrap(),
-            }),
-            "pop" => CommandType::C_POP(ArgumentPair {
-                first: command_list
-                    .next()
-                    .expect("First argument of pop is expected!"),
-                second: command_list
-                    .next()
-                    .expect("Second argument of pop is expected!")
-                    .parse()
-                    .unwrap(),
-            }),
-            _ => panic!("Couldn't decipher instruction type"),
-        }
-    }
-
-    /// Returns first argument of current command
-    /// # Panics
-    /// Dont call for C_RETURN type
-    pub fn arg1(self) -> &'a str {
-        match self.command_type() {
-            CommandType::C_RETURN => panic!("Arg 1 can't be called with RETURN type"),
-            CommandType::C_ARITHMETIC(command) => command,
-            CommandType::C_PUSH(argument_pair) => argument_pair.first,
-            _ => "",
-        }
-    }
-
-    /// Returns second argument of current command
-    /// # Panics
-    /// Only call if current command type
-    /// is C_PUSH, C_CALL, C_POP, C_FUNCTION
-    pub fn arg2(self) -> u128 {
-        match self.command_type() {
-            CommandType::C_PUSH(argument_pair) | CommandType::C_POP(argument_pair) => {
-                argument_pair.second
-            }
-            _ => panic!("Only call for C_PUSH, C_POP, C_CALL, C_FUNCTION commandType"),
-        }
-    }
+    // /// # Panics
+    // /// if command type can't be deciphered
+    // pub fn command_type(self) -> CommandType<'a> {
+    //     let mut command_list = self.current_command.split_ascii_whitespace();
+    //     let command = command_list
+    //         .next()
+    //         .expect("Command section of current instruction couldn't be extracted");
+    //
+    //     match command {
+    //         "and" | "or" | "sub" | "eq" | "neg" | "gt" | "lt" | "not" | "add" => {
+    //             CommandType::C_ARITHMETIC("")
+    //         }
+    //         // "push" => CommandType::C_PUSH(ArgumentPair {
+    //         //     first: command_list
+    //         //         .next()
+    //         //         .expect("First argument of push is expected!"),
+    //         //     second: command_list
+    //         //         .next()
+    //         //         .expect("Second argument of push is expected!")
+    //         //         .parse()
+    //         //         .unwrap(),
+    //         // }),
+    //         // "pop" => CommandType::C_POP(ArgumentPair {
+    //         //     first: command_list
+    //         //         .next()
+    //         //         .expect("First argument of pop is expected!"),
+    //         //     second: command_list
+    //         //         .next()
+    //         //         .expect("Second argument of pop is expected!")
+    //         //         .parse()
+    //         //         .unwrap(),
+    //         // }),
+    //         _ => panic!("Couldn't decipher instruction type"),
+    //     }
+    // }
+    //
+    // /// Returns first argument of current command
+    // /// # Panics
+    // /// Dont call for C_RETURN type
+    // // pub fn arg1(&self) -> &str {
+    // //     match self.command_type() {
+    // //         CommandType::C_RETURN => panic!("Arg 1 can't be called with RETURN type"),
+    // //         CommandType::C_ARITHMETIC(command) => command,
+    // //         CommandType::C_PUSH(argument_pair) => argument_pair.first,
+    // //         _ => "",
+    // //     }
+    // // }
+    //
+    // /// Returns second argument of current command
+    // /// # Panics
+    // /// Only call if current command type
+    // /// is C_PUSH, C_CALL, C_POP, C_FUNCTION
+    // pub fn arg2(self) -> u128 {
+    //     match self.command_type() {
+    //         CommandType::C_PUSH(argument_pair) | CommandType::C_POP(argument_pair) => {
+    //             argument_pair.second
+    //         }
+    //         _ => panic!("Only call for C_PUSH, C_POP, C_CALL, C_FUNCTION commandType"),
+    //     }
+    // }
 }
 
 #[cfg(test)]
